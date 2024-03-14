cc2b8b83496b0c1fa6847dd3fab55ffd33877984
d13751200344 <d13751200344@gmail.com>
2024-03-14 01:53:29 -0400

    ViewModel

--- app/src/main/java/com/example/jwassignment2/ViewModel/PlayerViewModel.java
+++ app/src/main/java/com/example/jwassignment2/ViewModel/PlayerViewModel.java
@@ -3,5 +3,29 @@ package com.example.jwassignment2.ViewModel;
 import androidx.lifecycle.ViewModel;
 
 public class PlayerViewModel extends ViewModel {
+    private String playerOneName;
+    private String playerTwoName;
 
+    //constructor
+    public PlayerViewModel() {
+    }
+
+    public String getPlayerOneNameName() {
+        return playerOneName;
+    }
+    public String getPlayerTwoNameName() {
+        return playerTwoName;
+    }
+
+    public void setPlayerOneName(String playerOneName) {
+        this.playerOneName = playerOneName;
+    }
+    public void setPlayerTwoName(String playerTwoName) {
+        this.playerTwoName = playerTwoName;
+    }
+
+    // Method to validate player names (optional)
+    public boolean arePlayerNamesValid() {
+        return !playerOneName.isEmpty() && !playerTwoName.isEmpty();
+    }
 }
